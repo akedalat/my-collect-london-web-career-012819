421c4ec18f57fabd130a2f1056254dc853c55413
Kian Edalat <akedalat@gmail.com>
2019-01-13 21:13:43 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,8 +2,8 @@ def my_collect (array)
   new_array = []
   i = 0
   while i < array.length
-    yield(array[i])
-    new_array.push(array[i])
+    yield(new_array.push(array[i]))
+    
 i += 1
   end
 new_array
